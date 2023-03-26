5be04ed122f2ba0d436325511d7422fb793ed14c
Philippe Nadeau <phil@MacBook-Pro.local>
2023-03-25 23:19:43 -0400

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [choice, setChoice] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const generateImage = async () => {
+    const prompt = choice === '1' ? 'Une scène mystérieuse inspirée de Lovecraft avec le Dr. Morgan' : 'Une scène mystérieuse inspirée de Lovecraft enquêtant sur la famille Morgan';
+    // Remplacez cette URL par l'URL de votre API
+    const response = await axios.post('https://your-api-url.com/generate-image', { prompt });
+    setImageUrl(response.data.imageUrl);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jeu inspiré de Lovecraft</h1>
+      <div>
+        <label>
+          <input type="radio" name="choice" value="1" onChange={(e) => setChoice(e.target.value)} />
+          Rencontrer le Dr. Morgan
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="radio" name="choice" value="2" onChange={(e) => setChoice(e.target.value)} />
+          Enquêter sur le Dr. Morgan et sa famille
+        </label>
+      </div>
+      <button onClick={generateImage} disabled={!choice}>
+        Faites votre choix
+      </button>
+      {imageUrl && (
+        <>
+          <h2>Scène :</h2>
+          <img src={imageUrl} alt="Scène générée" />
+        </>
+      )}
     </div>
   );
 }
